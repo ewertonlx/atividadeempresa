708509639c5494b10754acf34f5b5e7e091a8053
ewertonlx <ewerton123ofc@gmail.com>
2024-10-28 19:16:08 -0300

    atividade

--- bd_sql/tabelas.sql
+++ bd_sql/tabelas.sql
@@ -8,10 +8,14 @@ CREATE TABLE empresas (
 CREATE TABLE politicas_empresa (
     nome_empresa VARCHAR(255),
     nome_politica VARCHAR(255),
+    idPolitica INT NOT NULL,
     PRIMARY KEY (nome_empresa, nome_politica),
-    FOREIGN KEY (nome_empresa) REFERENCES empresas(nome)
+    FOREIGN KEY (nome_empresa) REFERENCES empresas(nome),
+    FOREIGN KEY (idPolitica) REFERENCES politicas_antirracistas(idPolitica)
 );
 
 CREATE TABLE politicas_antirracistas (
-    
+    idPolitica INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) UNIQUE,
+    descricao TEXT
 );
